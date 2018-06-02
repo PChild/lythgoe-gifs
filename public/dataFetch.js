0534f705c5e539de03b6c630babc7944005131d1
Preston <childrep@vt.edu>
2018-06-02 11:39:09 -0400

    Was on wrong bootstrap version

--- public/dataFetch.js
+++ public/dataFetch.js
@@ -4,11 +4,17 @@ $.get("https://api.apify.com/v1/RsXxWaYMxHPjuX3q9/crawlers/e8Lg5LqmfPgcY5eDy/las
 
   let gifPosts = data.filter(item => item.hasGif);
   let textPosts = data.filter(item => !item.hasGif);
+  let template = Handlebars.compile($('#gifTemplate').html());
 
   $(gifPosts).each(i => {
-    $('#gifList').append('<div class="card" style="width: 18rem;"><img class="card-img-top" src="' + gifPosts[i].gifAddress + '" alt="Card image cap"><div class="card-body"><h5 class="card-title">' + gifPosts[i].date + '</h5></div></div><br>')
+    let element = template({
+      "imgSrc": gifPosts[i].gifAddress,
+      "postDate": gifPosts[i].date
+    });
+
+    $('#gifList').append(element);
   });
 
-  $('#statList').append("<h3><li>In his last 200 posts Matt has posted " + gifPosts.length + " gifs and " + textPosts.length + " text posts.</li></h3>");
-  $('#statList').append("<h3><li>Matt's last text post was: <i>\"" + textPosts[0].text + "\"</i> It was posted: " + textPosts[0].date + ".</li></h3>");
+  $('#statList').append("<h4><li>In his last 200 posts Matt has posted " + gifPosts.length + " gifs and " + textPosts.length + " text posts.</li></h4>");
+  $('#statList').append("<h4><li>Matt's last text post was: <i>\"" + textPosts[0].text + "\"</i> Posted: " + textPosts[0].date + ".</li></h4>");
 })
