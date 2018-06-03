d597b9fcd6e97f266d4a7c787bc2977aa233597f
Preston <childrep@vt.edu>
2018-06-03 11:54:19 -0400

    Updated stat template, moved to pulling 500 posts

--- public/dataFetch.js
+++ public/dataFetch.js
@@ -3,25 +3,39 @@ function findDuplicates(gifPosts) {
 }
 
 $.get("https://api.apify.com/v1/RsXxWaYMxHPjuX3q9/crawlers/e8Lg5LqmfPgcY5eDy/lastExec/results?token=qhmGuje8Kp6npxmfs2fQMYAQu", function (baseData) {
-  let data = baseData[0].pageFunctionResult;
+  let data = [...baseData[0].pageFunctionResult, ...baseData[1].pageFunctionResult, ...baseData[2].pageFunctionResult]
+
+
+  console.log(data);
 
   let gifPosts = data.filter(item => item.hasGif);
   let textPosts = data.filter(item => !item.hasGif);
   let gifTemplate = Handlebars.compile($('#gifTemplate').html());
   let postTemplate = Handlebars.compile($('#postTemplate').html());
 
-  let element = postTemplate({
+  let lastPostElement = postTemplate({
+    leaderText: "Matt's latest text post was: ",
     postText: textPosts[0].text,
     postDate: textPosts[0].date,
     threadLink: textPosts[0].threadLink,
     threadTitle: textPosts[0].threadTitle
   });
 
+  let last = textPosts.length - 1;
+  let earlyPostElement = postTemplate({
+    leaderText: "Matt's earliest captured post was: ",
+    postText: textPosts[last].text,
+    postDate: textPosts[last].date,
+    threadLink: textPosts[last].threadLink,
+    threadTitle: textPosts[last].threadTitle
+  });
+
   let uniqueGifs = [...new Set(gifPosts.map(item => item.gifAddress.substr(14)))].length
 
-  $('#statList').append("<h4><li>In his last 200 posts Matt has posted " + gifPosts.length + " GIFs and " + textPosts.length + " text posts.</li></h4>");
+  $('#statList').append("<h4><li>In his last " + data.length + " posts Matt has posted " + gifPosts.length + " GIFs and " + textPosts.length + " text posts.</li></h4>");
   $('#statList').append("<h4><li>" + uniqueGifs + " out of " + gifPosts.length + " of Matt's GIFs have been unique.</li></h4>");
-  $('#statList').append(element);
+  $('#statList').append(lastPostElement);
+  $('#statList').append(earlyPostElement)
 
   $(gifPosts).each(i => {
     let element = gifTemplate({
